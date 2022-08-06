42a52814d82a1f48740b2fd9ecb74b9dbd860948
Chris Bohlman <cbohlman@pm.me>
2022-08-06 16:57:26 -0700

    Finished project

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '152f9a42a6msh939296488735370p122a08jsn90cea057a810',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "432a892e114b64ddd9e2e76c91f9076a"
